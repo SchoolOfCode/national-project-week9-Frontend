76210d316b8079633dd056f8f4d3e479a2c4f6c8
tomdichmont <t.dichmont@gmail.com>
2022-01-18 10:17:01 +0000

    Route 1, components drafted

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import Header from '../Header';
 import './App.css';
+import TopicSection from "../TopicSection";
 
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <TopicSection></TopicSection>
     </div>
   );
 }
---
+++ src/components/TopicSection/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Topic from "../Topic";
+
+export default function TopicSection() {
+    return (
+        <div>
+            <p>Topic Section</p>
+            <Topic></Topic>
+            <Topic></Topic>
+            <Topic></Topic>
+        </div>
+    )
+}
