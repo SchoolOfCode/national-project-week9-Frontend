45dcdc3382ec6799af5879ac0cf5b4fde1d953d3
unknown <kavita.kaur998@gmail.com>
2022-01-18 16:39:13 +0000

    data to be displayed in correct format

--- src/components/QuestionsPage/index.js
+++ src/components/QuestionsPage/index.js
@@ -6,6 +6,7 @@ import { useParams } from "react-router-dom";
 import mockData from "../../lib";
 
 export default function QuestionsPage() {
+  const [data, setData] = useState("");
   const params = useParams();
   const topic = params.query;
   console.log(topic);
@@ -13,23 +14,33 @@ export default function QuestionsPage() {
   useEffect(() => {
     async function getQuestions(topic) {
       try {
-        const response = await fetch(`http://localhost:5000/questions?topic=${topic}`)
-        const questionData = await response.json();
-        console.log(questionData)
-      } catch(error) {}
+        const response = await fetch(
+          `http://localhost:5000/questions?topic=${topic}`
+        );
+        let questionData = await response.json();
+        setData(questionData.payload.slice(0, 6));
+      } catch (error) {}
     }
-    getQuestions(topic)
-  }, [topic])
+    getQuestions(topic);
+  }, [topic]);
 
-
+  if (data) {
+    console.log(data);
+  }
   return (
     <div className="question_Section">
       <form>
-        <p>Questions</p>
-        {mockData.payload.map((obj) => {
-          return <Question key={obj.id} question={obj.question}></Question>;
-        })}
-        <input type="submit"></input>
+        {data.map(({correct_answer, incorrect_answers, question, topic, id}) => (
+          <div className="QuestionBox" key={id}>
+          <p>{question}</p>
+          <input type="radio">{correct_answer}</input>
+          {incorrect_answers.map((ans) => (
+             <input type="radio">{ans}</input>
+          ))}
+          </div>
+        ))}
+
+        <button type="submit"></button>
       </form>
     </div>
   );
